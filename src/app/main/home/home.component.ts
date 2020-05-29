b6903904640c434d7f295b52ff4b49848f472230
Carlos lopez <carloslopezandres1997@gmail.com>
2020-05-28 21:33:05 -0500

    Add files via upload

---
+++ src/app/main/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  bandera=true;
+  home="home";
+  heroes=[{
+    "n":"batman",
+    "img": "https://cnnespanol2.files.wordpress.com/2019/12/s_64a163f16ecbb099e52f2f8271f73cbbfcfc9034be4d646f7375e4db1ca6f3d7_1573501883482_ap_19001106049831-1.jpg?quality=100&strip=info&w=320&h=240&crop=1"
+  },{
+    "n":"sp",
+    "img": "https://cnnespanol2.files.wordpress.com/2019/12/s_64a163f16ecbb099e52f2f8271f73cbbfcfc9034be4d646f7375e4db1ca6f3d7_1573501883482_ap_19001106049831-1.jpg?quality=100&strip=info&w=320&h=240&crop=1"
+  }];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
